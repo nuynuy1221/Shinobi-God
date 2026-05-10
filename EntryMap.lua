4930f7a5512ba55b8cf5a62e1c4b5b795a75db4e
nuynuy1221 <nuynuy1221@gmail.com>
2026-05-10 10:15:05 +0700

    Update EntryMap.lua

--- EntryMap.lua
+++ EntryMap.lua
@@ -489,6 +489,25 @@ task.spawn(function()
                 print("⏭️ ข้าม Enemy Milestone เพราะปิด CustomRR")
             end
 
+			if level < 11 then
+				local Add = {
+    				[1] = "AddMatch",
+    				[2] = {
+        				["Difficulty"] = "Normal",
+        				["Act"] = "Act1",
+        				["StageType"] = "Story",
+        				["Stage"] = "Stage1",
+        				["FriendsOnly"] = false
+    				}
+				}
+				game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("LobbyEvent"):FireServer(unpack(Add))
+				task.wait(2)
+				local ST = {
+    				[1] = "StartMatch"
+				}
+				game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("LobbyEvent"):FireServer(unpack(ST))
+			end
+
             -- ❌ ไม่เข้า Story แล้ว ไม่สน Level
             -- ทำแต่ Winter เท่านั้น
 
