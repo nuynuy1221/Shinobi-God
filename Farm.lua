9cb64cf9ea488dc3fa090550e05e14968f73de5e
nuynuy1221 <nuynuy1221@gmail.com>
2026-05-19 06:03:01 +0700

    Update Farm.lua

--- Farm.lua
+++ Farm.lua
@@ -801,7 +801,7 @@ task.spawn(function()
         -- WAVE 5
         if wave >= 5 and not Executed[5] then
             Executed[5] = true
-            task.wait(20)
+            task.wait(25)
             local SKIP5 = {
                 [1] = "Purchase",
                 [2] = "SkipWaves5"
